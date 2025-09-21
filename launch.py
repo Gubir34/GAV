05d3c7ab3517f330b2c42bf643c51260e1892e8d
Gubir34 <canteoman15@gmail.com>
2025-09-21 18:00:05 +0300

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,13 +1,25 @@
 import subprocess
 import platform
+import os
+import urllib.request
 from lang_loader import LangLoader
 
 lang = LangLoader("lng.ltf", default="en")
+
+INPUT_FILE = "hashes_raw.txt"
+
+HASHES_URL = "https://edef1.pcloud.com/cBZwzHz1OZUoETr27ZZZGhYy0kZ2ZZSa0ZkZu7cILZ9zZhHZV8ZV0ZhpZMXZvLZK4ZKRZkpZwFZckZIkZNzZ5kyUZTBettr1UrquDWYYw6lsirFgXUSJV/hashes_raw.txt"
+
 
 def run_recompile():
     print(lang.t("recompile_start"))
     subprocess.run(["python3" if platform.system() != "Windows" else "py", "recompile.py"], check=True)
     print(lang.t("recompile_done"))
+
+if not os.path.exists(INPUT_FILE):
+    print("[!] hashes_raw.txt not found, downloading...")
+    urllib.request.urlretrieve(HASHES_URL, INPUT_FILE)
+    print("[+] Download completed: hashes_raw.txt")
 
 def run_import_hashes():
     print(lang.t("import_hashes_start"))
@@ -28,3 +40,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
